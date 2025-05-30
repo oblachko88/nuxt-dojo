cbc78504c5d642f19890408db91e682ca2703f01
oblachko88 <oblachko88@gmail.com>
2025-05-30 19:43:32 +0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline text-primary">Hello world!</h1>
+    <h2>Home</h2>
+    <p class="bg-primary text-neutral-300 p-2 border border-gray-500 rounded-xl shadow-2xl shadow-red-500 mb-10 text-xl hover:bg-red-500 transition duration-500 hover:p-6">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus
+      ipsam quis laborum iure accusantium illo soluta a minus iste consequatur.
+    </p>                                                                                  vbn
+    <p class="md:text-red-500                                                  lg:text-green-500">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus
+      ipsam quis laborum iure accusantium illo soluta a minus iste consequatur.
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+</style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>Product details for {{ id }}</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis sit earum cum repellat eius eum quis maxime omnis dolor porro!</p>
+    </div>
+</template>
+
+<script setup>
+    const { id } = useRoute().params
+</script>
+
+<style scoped>
+
+</style>
